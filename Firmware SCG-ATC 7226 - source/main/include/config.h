59b7f9cd6f0a4a11d76a92bab8b25d5f12a303b9
Krzysztof Dymianiuk <dvjcodec@gmail.com>
2020-09-15 21:10:01 +0200

    Delete config.h

--- Firmware SCG-ATC 7226 - source/main/include/config.h
+++
@@ -1,9 +0,0 @@
-
-struct ConfigParam
-{
-    int boostMax, boostDrop, boostSpring, fuelMaxRPM, maxRPM, tireWidth, tireProfile, tireInches, rearDiffTeeth, nextShiftDelay, stallSpeed, batteryLimit, firstTccGear, triggerWheelTeeth, tpsAgre, highRPMshiftLimit, lowRPMshiftLimit;
-    float diffRatio, maxSlip, transSloppy;
-    int oneTotwo, twoTothree, threeTofour, fourTofive, fiveTofour, fourTothree, threeTotwo, twoToone;
-};
-
-extern struct ConfigParam config;
